3bd328c7f5cb1b3f1e0f4f3a241308299cbc4160
kwayoteng-cmd <kwayoteng@gmail.com>
2025-12-30 20:09:19 -0500

    Update app.py

--- 01-secure-ci-pipeline/app/app.py
+++ 01-secure-ci-pipeline/app/app.py
@@ -1,1 +1,11 @@
+from flask import Flask
+
+app = Flask(Matics_Chrome)
+
+@app.route("/")
+def home():
+    return "Secure CI Pipeline Running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
